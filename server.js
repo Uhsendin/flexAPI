d79172c14883ccd5bf1da1e3ad808282da728777
Uhsendin <chilebr99@gmail.com>
2023-06-15 14:37:18 -0400

    server sends index.html

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const PORT = 8000
 
 app.use(cors())
 
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}!`)
 })
